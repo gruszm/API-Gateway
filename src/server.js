428978ebe93fbfca56aaacc9ce22d7ef201c6c9b
gruszm <mariuszgruszczynski11@gmail.com>
2025-06-24 22:22:28 +0200

    Middleware's target correction after module rename

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(AuthMiddleware);
 app.use(createProxyMiddleware({ pathFilter: ["/api/public/products", "/api/secure/products"], target: "http://products_service:8081", changeOrigin: true }));
 app.use(createProxyMiddleware({ pathFilter: ["/api/public/carts", "/api/secure/carts"], target: "http://carts-service:8082", changeOrigin: true }));
-app.use(createProxyMiddleware({ pathFilter: ["/api/public/profiles", "/api/secure/profiles"], target: "http://profile-service:8083", changeOrigin: true }));
+app.use(createProxyMiddleware({ pathFilter: ["/api/public/profiles", "/api/secure/profiles"], target: "http://profiles-service:8083", changeOrigin: true }));
 app.use("/", gatewayRouter);
 
 MongoConnection.connect(process.env.USERS_DB_SERVICE_NAME).then(() => {
